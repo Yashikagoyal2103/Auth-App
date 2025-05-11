93b8ae47700890a7fd9be52627d4c4ba79567cb2
Yashika <yashikagoyal2005@gmail.com>
2025-05-11 19:03:36 +0530

    login and signup added

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,9 @@
 const bcrypt =require('bcrypt');
-const User =require('./models/User.js');
+const User =require('../model/User.js');
+const jwt =require('jsonwebtoken');
+require('dotenv').config();
 
+//signup
 exports.signup = async (req,res) => {
     try{
         const {name, email, password , role} = req.body;
@@ -48,6 +51,64 @@ exports.signup = async (req,res) => {
                 message:'Internal server error'
             }
         );
+    }
+}
+
+//login
+exports.login =async (req,res) => {
+    try{
+        const {email , password} = req.body;
+        //validation on email and password
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:'Please enter email and password'
+            })
+        }
+
+    const user =await User.findOne({email});
+    if(!user){
+        return res.status(401).json({
+                success:false,
+                message:'User not registered'
+        })
+    }
+
+    const payload ={
+        email:user.email,
+        id:user._id,
+        role:user.role
+    }
+    //password varification and jwt token generation
+    if(await bcrypt.compare(password , user.password)){
+        const token = jwt.sign( payload ,process.env.JWT_SECRET, process.env.JWT_EXPIRY);
+        user.token =token;
+        user.password =undefined;
+
+        //cookies
+        const options = {
+            expires: new Date(Date.now() + 3*24*60*60*1000),
+            httpOnly:true
+        } 
+        res.cookie('token', token , options).status(200).json({
+            success:true,
+            message:'Login successful',
+            user,
+            token
+        })
+    }
+    else{
+        return res.status(403).json({
+                success:false,
+                message:'Password is incorrect'
+            })
+    }
+
+    }catch(err) {
+        res.status(500).json({
+            success:false,
+            message:'Internal server error'
+        })
     }
 }
     
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router =express.Router();
 
-const {login , signup} =require('./controllers/user.js');
+const {login , signup} =require('../controllers/Auth.js');
 
 router.post('/login',login);
 router.post('/signup',signup);
 
 module.exports=router;
+
